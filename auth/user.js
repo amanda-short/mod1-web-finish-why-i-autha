cc74ceaa8ae58089a3de77b84f69623571f8e8e2
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-09-26 14:49:35 -0700

    Part B - Sign out complete

--- auth/user.js
+++ auth/user.js
@@ -8,4 +8,30 @@ const signOutLink = document.getElementById('sign-out-link');
 //     - check if there is not a user and if so do the redirect
 //       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
 
+let error = null;
+let profile = null;
+
 // > Part B: attach event listener to signOutLink that calls signOutUser
+signOutLink.addEventListener('click', signOutUser);
+
+window.addEventListener('load', async () => {
+    const response = await getProfile(user.id);
+    error = response.error;
+    profile = response.data;
+
+    if (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return;
+    }
+
+    if (profile) {
+        displayProfile();
+    }
+});
+
+function displayProfile() {
+    if (userAvatar && profile && profile.avatar_url) {
+        userAvatar.src = profile.avatar_url;
+    }
+}
